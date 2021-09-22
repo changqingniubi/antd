b424c01165011479cad5ab24f6a82bb96b1ff360
changqing <qing.chang@eeoa.com>
2021-09-22 23:33:10 +0800

    fix: test

--- components/button/button.tsx
+++ components/button/button.tsx
@@ -2,7 +2,7 @@
  * @Description:
  * @Author: changqing
  * @Date: 2021-09-21 16:03:53
- * @LastEditTime: 2021-09-21 18:21:51
+ * @LastEditTime: 2021-09-22 23:29:53
  * @LastEditors: changqing
  * @Usage:
  */
@@ -11,7 +11,11 @@ import React, { ButtonHTMLAttributes } from 'react';
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {}
 const Button: React.FC<ButtonProps> = (props) => {
   const { children } = props;
-  return <button type="button">{children}</button>;
+  return (
+    <button type="button" className="default">
+      {children}
+    </button>
+  );
 };
 export default Button;
 // 如果你导出的是type，会保证在编译去掉，可以进行更好的优化
